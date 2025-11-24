c33f60444b12faf089e63b9c815ed83d9f6c02cc
abual3izk <abdalazizkuwair@gmail.com>
2025-11-25 00:32:17 +0300

    Add project files.

---
+++ ProjectUniconnect/Controllers/HomeUniconnectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectUniconnect.Controllers
+{
+    public class HomeUniconnectController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
